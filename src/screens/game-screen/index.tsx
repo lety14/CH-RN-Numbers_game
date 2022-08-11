8938ceb32298ac4f277ab35ef05dad904f112f56
Leti <leticiapistarelli@gmail.com>
2022-08-11 14:10:08 -0300

    create game-screen

---
+++ src/screens/game-screen/index.tsx
@@ -0,0 +1,32 @@
+import React, { FC, useState } from "react";
+import { Button, View, Text } from "react-native";
+import { styles } from "./styles";
+import colors from "../../constants/colors";
+import Card from "../../components/card";
+import NumberContainer from "../../components/number-container";
+import { generateRandomNumber } from "../../utils/generateRandomNumber";
+
+type Props = {
+  userNumber: number;
+};
+
+const GameScreen: FC<Props> = ({ userNumber }) => {
+  const [currentNumber, setCurrentNumber] = useState<number>(
+    generateRandomNumber(0, 100, userNumber)
+  );
+
+  return (
+    <View style={styles.screen}>
+      {/* <Card style={styles.card}> */}
+      <Text>La suposicion del oponente</Text>
+      <NumberContainer number={currentNumber} />
+      <View style={styles.buttonContainer}>
+        <Button title="Menor" color={colors.primary} onPress={() => null} />
+        <Button title="Mayor" color={colors.primary} onPress={() => null} />
+      </View>
+      {/* </Card> */}
+    </View>
+  );
+};
+
+export default GameScreen;
---
+++ src/screens/game-screen/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+import colors from "../../constants/colors";
+
+export const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
